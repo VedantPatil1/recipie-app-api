8609bc242550730eace1bcdba43cda09e25829d9
Vedant Patil <vedantpatil.w.1@gmail.com>
2023-01-29 00:05:11 +0530

    Added postgresql service and pyscopg2 adaptor installation

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions.
+"""
+
+def Add(x,y):
+    """Adds x and y and returns result."""
+    sum = x+y
+    return sum
+
+def Sub(x,y):
+    """Subtracts y from x and returns result."""
+    sub = x-y
+    return sub
