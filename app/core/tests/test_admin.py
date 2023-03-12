0d94ada38de04f17da430e138892dc36406befef
Vedant Patil <vedantpatil.w.1@gmail.com>
2023-03-12 16:20:38 +0530

    Successfully configured Django admin Page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -35,4 +35,11 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         res = self.client.get(url)
 
-        self.assertEqual(res.client_code, 200)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create use page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
