336112e9c6cebccc0de8b6b472abdc3a660c019f
Vedant Patil <vedantpatil.w.1@gmail.com>
2023-03-14 11:50:20 +0530

    HTTP create user method added

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -33,7 +33,7 @@ class PublicUserAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(email=payload['email'])
         self.assertTrue(user.check_password(payload['password']))
-        self.assertNotIn('password', res.date)
+        self.assertNotIn('password', res.data)
 
     def test_user_with_email_exists_error(self):
         """Test error returned if user with email exists."""
